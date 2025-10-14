bc1588cec5c3628d39ac3f1c879fc829be862578
ivanlukomskiy <ivanlukomskiy@avride.ai>
2025-10-14 19:38:20 +0400

    0 errors p70

--- src/processing/config.ts
+++ src/processing/config.ts
@@ -21,11 +21,16 @@ export const DctConfs: DctCoefConf[] = [
     {x: 1, y: 0, bitsCapacity: 3},
     {x: 0, y: 1, bitsCapacity: 3},
     {x: 1, y: 1, bitsCapacity: 3},
-    {x: 2, y: 0, bitsCapacity: 4},
-    {x: 0, y: 2, bitsCapacity: 4},
+    {x: 2, y: 0, bitsCapacity: 3},
+    {x: 0, y: 2, bitsCapacity: 3},
     {x: 2, y: 2, bitsCapacity: 3},
     {x: 3, y: 0, bitsCapacity: 1},
     {x: 0, y: 3, bitsCapacity: 1},
+    {x: 2, y: 1, bitsCapacity: 1},
+    {x: 1, y: 2, bitsCapacity: 1},
+
+    {x: 1, y: 3, bitsCapacity: 1},
+    {x: 3, y: 1, bitsCapacity: 1},
 ] // 24
 
 // 3 * 4 + 2 -> 14 bytes?
@@ -52,11 +57,11 @@ export const DefaultEncodingConf: EncodingConf = {
     // chromaConf: DctConfs,
     chromaConf: DctConfsChroma,
     lumaDctToImageTransform: {
-        multiplier: .3,
+        multiplier: .25,
         addition: .3,
     },
     chromaDctToImageTransform: {
-        multiplier: .3,
+        multiplier: .25,
         // multiplier: .0,
         addition: .5,
     }
