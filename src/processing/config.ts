6b3eddae0448db45f0d9bbd15aa3ec7753870cd5
ivanlukomskiy <ivanlukomskiy@avride.ai>
2025-10-14 19:33:06 +0400

    better

--- src/processing/config.ts
+++ src/processing/config.ts
@@ -17,13 +17,15 @@ export interface EncodingConf {
 }
 
 export const DctConfs: DctCoefConf[] = [
-    {x: 0, y: 0, bitsCapacity: 4},
-    {x: 1, y: 0, bitsCapacity: 4},
-    {x: 0, y: 1, bitsCapacity: 4},
-    {x: 1, y: 1, bitsCapacity: 4},
+    {x: 0, y: 0, bitsCapacity: 2},
+    {x: 1, y: 0, bitsCapacity: 3},
+    {x: 0, y: 1, bitsCapacity: 3},
+    {x: 1, y: 1, bitsCapacity: 3},
     {x: 2, y: 0, bitsCapacity: 4},
     {x: 0, y: 2, bitsCapacity: 4},
-    // {x: 2, y: 2, bitsCapacity: 2},
+    {x: 2, y: 2, bitsCapacity: 3},
+    {x: 3, y: 0, bitsCapacity: 1},
+    {x: 0, y: 3, bitsCapacity: 1},
 ] // 24
 
 // 3 * 4 + 2 -> 14 bytes?
@@ -50,11 +52,11 @@ export const DefaultEncodingConf: EncodingConf = {
     // chromaConf: DctConfs,
     chromaConf: DctConfsChroma,
     lumaDctToImageTransform: {
-        multiplier: .35,
+        multiplier: .3,
         addition: .3,
     },
     chromaDctToImageTransform: {
-        multiplier: .35,
+        multiplier: .3,
         // multiplier: .0,
         addition: .5,
     }
