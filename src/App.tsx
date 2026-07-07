10f8d7e3a5463b9118bad9c48f71079317c46bb6
ivan <ivanlukomskiy@gmail.com>
2026-07-08 00:47:24 +0200

    ditch calib page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Benchmark } from './pages/benchmark/benchmark';
 import { AppShell, Burger, Flex, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { EncodeFile } from './pages/encode-file/encode_file';
-import { Calibrate } from './pages/calibrate/calibrate';
 
 function App() {
   const [opened, { toggle }] = useDisclosure();
@@ -32,7 +31,6 @@ function App() {
         >
           <Flex direction={'row'} gap={'xl'}>
             <Link to="/">file</Link>
-            <Link to="/calibrate">calibrate</Link>
             <Link to="/benchmark">benchmark</Link>
             <Link to="/block">single block</Link>
           </Flex>
@@ -53,9 +51,6 @@ function App() {
         <Link to="/" style={{ padding: 16 }} onClick={toggle}>
           file
         </Link>
-        <Link to="/calibrate" style={{ padding: 16 }} onClick={toggle}>
-          calibrate
-        </Link>
         <Link to="/benchmark" style={{ padding: 16 }} onClick={toggle}>
           benchmark
         </Link>
@@ -70,7 +65,6 @@ function App() {
       <AppShell.Main>
         <Routes>
           <Route path="/" element={<EncodeFile />} />
-          <Route path="/calibrate" element={<Calibrate />} />
           <Route path="/benchmark" element={<Benchmark />} />
           <Route path="/block" element={<OneBlockTest />} />
         </Routes>
